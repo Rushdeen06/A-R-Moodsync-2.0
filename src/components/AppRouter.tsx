23e6760102cf484b9dc927264d6e8075d3740c69
Rushdeen06 <rushdeen.white@mrisoftware.com>
2025-10-31 09:25:16 +0200

    Fix TypeScript error: Remove unused Navigate import

--- src/components/AppRouter.tsx
+++ src/components/AppRouter.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Routes, Route, Navigate } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import MoodChannelView from './MoodChannelView'
 import AnalyticsPage from '../pages/AnalyticsPage'
 import TeamMembersPage from '../pages/TeamMembersPage'
